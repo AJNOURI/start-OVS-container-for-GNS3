f038e4ba7f5a90d8c5a175bdfe194601b07b7974
ajnouri <ajn.bin@gmail.com>
2015-08-10 12:00:05 +0200

    bridge name correction

--- start-ovs.sh
+++ start-ovs.sh
@@ -8,10 +8,10 @@ sudo pipework br201 -i eth1 $CID 0/0
 sudo pipework br202 -i eth2 $CID 0/0
 sudo pipework br203 -i eth3 $CID 0/0
 sudo pipework br204 -i eth4 $CID 0/0
-sudo pipework br105 -i eth5 $CID 0/0
-sudo pipework br106 -i eth6 $CID 0/0
-sudo pipework br107 -i eth7 $CID 0/0
-sudo pipework br108 -i eth8 $CID 0/0
+sudo pipework br205 -i eth5 $CID 0/0
+sudo pipework br206 -i eth6 $CID 0/0
+sudo pipework br207 -i eth7 $CID 0/0
+sudo pipework br208 -i eth8 $CID 0/0
 
 sudo docker exec -it $CID /bin/sh
 
